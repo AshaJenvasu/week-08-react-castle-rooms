0ce855f77c41151fb53bb32175e5b87c30c0d6dc
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-21 14:58:06 +0700

    Add: set up all file jsx with prototype castle

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import Castle from "./components/Castle.jsx";
+import Castle from "./components/01_Castle.jsx";
+import { useState } from "react";
+import SecretRoom from "./components/09_SecretRoom.jsx";
 
 export default function App() {
+  const [question, setQuestion] = useState();
+  const [answer, setAnswer] = useState();
+  const handleQuestion = (e) => {
+    console.log(e);
+    setQuestion(e.target.value);
+  };
+
+  const handleAnswer = (e) => {
+    console.log(e);
+    setAnswer(e.target.value);
+  };
+
   return (
     <div className="pb-80 py-10 gap-y-4 flex flex-col justify-center items-center min-h-screen bg-gray-800 text-white">
       <p className="text-purple-300">Message for JSD12:</p>
 
       <span className="text-yellow-300">
-        {/* question or waiting for a message */}
+        {/* question or waiti ng for a message */}
+        {question ? question : "Waiting for a message..."}
       </span>
 
       <textarea
-        value="banana"
-        onChange=""
+        value={question}
+        onChange={handleQuestion}
         className="bg-white text-black rounded px-2 py-1"
         placeholder="Type your message here..."
       />
@@ -21,7 +36,7 @@ export default function App() {
           {/* reply or waiting for a reply */}
         </span>
       </p>
-      <Castle />
+      <Castle question={question} answer={answer} handleAnswer={handleAnswer} />
     </div>
   );
 }
---
+++ src/components/09_SecretRoom.jsx
@@ -0,0 +1,21 @@
+export default function SecretRoom({ question, answer, handleAnswer }) {
+  return (
+    <div className="flex flex-col justify-center items-center pt-10 bg-gray-700 w-[90%]">
+      <h1 className="font-bold text-4xl p-5">SecretRoom</h1>
+      <p>
+        <span> {question ? question : " ⏳ Waiting for a message..."}</span>
+      </p>
+      <p className="py-5">Reply to the outside 👨‍💻:</p>
+      <textarea
+        value={answer}
+        onChange={handleAnswer}
+        className="bg-white text-black rounded px-2 py-1"
+        placeholder="Type your reply here..."
+      />
+      <p className="py-5">
+        {" "}
+        <span> {answer ? answer : " ⏳ Waiting for a message..."}</span>
+      </p>
+    </div>
+  );
+}
